59155314f9941d4935c4598b0e475d874d2c91d6
Michelle Hachicho <michellehachicho@hotmail.com>
2013-09-28 10:40:20 -0400

    new tweets view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,18 @@
 class TweetsController < ApplicationController
+  before_filter :authenticate_user!, only: [:create, :new]
+ def index
+   @tweets = Tweet.all.order(:created_at => :desc)
+ end
+ 	def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    if @tweet = Tweet.create(body: params[:tweet][:body], user_id: current_user.id )
+      redirect_to tweets_path
+    else
+      render :new
+    end
+  end
+
 end
