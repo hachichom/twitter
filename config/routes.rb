70f8cb9959f4a36c4d326a347722dbab70d7cd8e
Michelle Hachicho <michellehachicho@hotmail.com>
2013-09-28 10:04:44 -0400

    created controller tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Twitter::Application.routes.draw do
   devise_for :users
-  resources :ideas
+  root 'tweets#index'
+  resources :tweets, only: [:new, :index, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
